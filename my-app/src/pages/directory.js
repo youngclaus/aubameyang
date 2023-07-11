4df653d47e36afda91557cdae1cef99947d2c5b8
cyoungclaus <dublecy@gmail.com>
2023-07-10 20:30:25 -0400

    updated title page

--- my-app/src/pages/directory.js
+++ my-app/src/pages/directory.js
@@ -1,14 +1,18 @@
-import React from "react";
-import MapContainer from "../Components/MapElement/map";
-import RestaurantList from "../Components/InfoDisplay/display";
+import React from "react"
+import MapContainer from "../Components/MapElement/map"
+import RestaurantList from "../Components/InfoDisplay/display"
+import Navbar from "../Components/Navbar"
 
 const Directory = () => {
-    return (
-        <div style={{backgroundColor:'#20bfd0'}}>
-            <MapContainer lat={40.744781} lng={-74.025789}/>
-            <RestaurantList />
-        </div>
-    );
-};
+  return (
+    <div style={{ backgroundColor: "#20bfd0" }}>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <MapContainer lat={40.744781} lng={-74.025789} />
+      <RestaurantList />
+    </div>
+  )
+}
 
-export default Directory;
+export default Directory
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import logo from "../img/logoIteration2.png";
-import './index.css';
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import logo from "../img/logoIteration2.png"
+import "./index.css"
 
 const Home = () => {
+  const [isHotspot, setHotspot] = useState(false)
+  const handleHotspot = () => {
+    setHotspot(true)
+    setTimeout(() => {
+      window.location.href = "Directory"
+    }, 1000)
+  }
   return (
     <div>
       <div className="app-background" />
-      <div className='app-logo-container'>
-        <img className="app-logo" src={logo} alt="logo"/>
+      <div className="app-logo-container">
+        <img className="app-logo" src={logo} alt="logo" />
+        <div
+          className={`hotspot-container ${isHotspot ? "zoomed" : ""}`}
+          onClick={handleHotspot}
+        >
+          <Link to="/Directory" className="hotspot-link" />
+        </div>
       </div>
-        
-      </div>
-  );
-};
-  
-export default Home;
+      <p className="app-title">click to enter!</p>
+    </div>
+  )
+}
+
+export default Home
